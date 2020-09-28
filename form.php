1bcc0c2525d16161ea6cbd8e378f103a04d8264a
Ivo <ivo@aquive.nl>
2020-09-28 14:31:11 +0200

    added google analytics to HEAD

--- form.php
+++ form.php
@@ -8,6 +8,19 @@
     <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous"></script>
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-179162367-1"></script>
+    <script>
+        window.dataLayer = window.dataLayer || [];
+
+        function gtag() {
+            dataLayer.push(arguments);
+        }
+        gtag('js', new Date());
+
+        gtag('config', 'UA-179162367-1');
+    </script>
+
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>La Gare - Laren</title>
 </head>
@@ -32,8 +45,7 @@
         fclose($fp); //Close the file to free memory.
 
         $infomessage = "Bedankt voor uw inschrijving! E&#233;n van onze makelaars neemt spoedig contact met u op.";
-    }
-    else {
+    } else {
         $infomessage = "Er ging iets verkeerd.";
     }
 
